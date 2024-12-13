66084c329dc23a040f3dd8024960fa7dc1ae844f
superhenryman <121818148+superhenryman@users.noreply.github.com>
2024-12-13 05:08:09 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, app
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+def worked():
+    return render_template('worked.html')
+@app.route('/submit', methods=['POST'])
+def gibmeyourmoney():
+    confession = request.form.get('confession')
+    if confession:
+        with open('confession.txt', 'a') as file:
+            file.write(confession  + '\n')
+        return worked(), 200
+    return "It didn't work :(", 400
+if __name__ == '__main__':
+    app.run(debug=True)
