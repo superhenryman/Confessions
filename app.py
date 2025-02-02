4e32d7bce8fffa793ee91d0ab817fe08086a7fad
superhenryman <121818148+superhenryman@users.noreply.github.com>
2025-02-02 23:45:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ import psycopg2
 import os
 from psycopg2 import sql
 import logging
+import requests
+import time
 app = Flask(__name__)
+SECRET_KEY = '6LeL18oqAAAAAAbzy3FKxobu2HOFB0Jgo_68xTJc'
 
+# Verify reCAPTCHA
+def verify_recaptcha(response_token):
+    url = 'https://www.google.com/recaptcha/api/siteverify'
+    data = {
+        'secret': SECRET_KEY,
+        'response': response_token
+    }
+    response = requests.post(url, data=data)
+    result = response.json()
+    return result.get('success')
 database_url = os.getenv('DATABASE_URL')
 
 if not database_url:
@@ -46,21 +59,23 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def gibmeyourmoney():
-    confession = request.form.get('confession')
-    if confession:
-        try:
-            conn = get_db_connection()
-            cur = conn.cursor()
-            cur.execute("INSERT INTO confessions (confession) VALUES (%s)", (confession,))
-            conn.commit()
-            cur.close()
-            conn.close()
-
-            return render_template('worked.html'), 200
-        except Exception as e:
-            app.logger.error(f"Error inserting confession: {e}")
-            return render_template('error.html'), 500
-    return "It didn't work :(", 400
+    response_token = request.form.get('g-recaptcha-response')
+    if verify_recaptcha(response_token):
+        # Continue processing form
+        confession = request.form.get('confession')
+        if confession:
+            try:
+                conn = get_db_connection()
+                cur = conn.cursor()
+                cur.execute("INSERT INTO confessions (confession) VALUES (%s)", (confession,))
+                conn.commit()
+                cur.close()
+                conn.close()
+                return render_template('worked.html'), 200
+            except Exception as e:
+                app.logger.error(f"Error inserting confession: {e}")
+                return render_template('error.html', errorcode = "Database didn't work.")
+        return "It didn't work :(", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
