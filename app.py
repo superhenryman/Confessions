57892b43466aeead697e5f89ba418b86b47afe90
= <=>
2025-03-29 20:48:59 +0300

    Add button for db to csv

--- app.py
+++ app.py
@@ -1,12 +1,50 @@
-from flask import Flask, render_template, request, logging
+from flask import Flask, render_template, request, logging, send_file
 import psycopg2
 import os
-from psycopg2 import sql
+from io import BytesIO
 import logging
 import requests
 import time
+import pandas as pd
 app = Flask(__name__)
 SECRET_KEY = os.getenv('SECRET_KEY')
+
+def Get_Data():
+    with get_db_connection() as conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM confessions")
+            data = cursor.fetchall()
+            allids = []
+            alldata = []
+            for row in data:
+                allids.append(row[0])
+                alldata.append(row[1])
+            result = {
+                "id": allids,
+                "confessions": alldata
+            }
+        #result = [{"id": row[0],  "confession": row[1]} for row in data]
+        except Exception as e:
+            raise Exception(f"Unhandled Exception when getting data: {e}")
+    return result
+
+def TurnIntoCSV(data):
+    try:
+        df = pd.DataFrame(data)
+        csv = df.to_csv(index=False)
+    except Exception as e:
+        raise Exception(f"Unhandled Exception at turning data to CSV format.: {e}")
+    return csv
+
+def Send_Data():
+    try:
+        data = Get_Data()
+        csv = TurnIntoCSV(data)
+        filestream = BytesIO(csv.encode())
+        return send_file(filestream, None, as_attachment=True, download_name="confessions.csv")
+    except Exception as e:
+        print(f"Unhandled Exception when sending data {e}") # to not lead to another exception with the route
 
 def verify_recaptcha(response_token):
     url = 'https://www.google.com/recaptcha/api/siteverify'
@@ -78,6 +116,14 @@ def gibmeyourmoney():
         return "It didn't work :(", 400
     else:
         return render_template("error.html", errorcode = "Captcha verification bad.")
+    
+@app.route("/getdata")
+def get_data():
+    try:
+        return Send_Data() # AHA!
+    except Exception as e:
+        raise Exception(f"Error while handling Send_Data() route. {e}")
+
 if __name__ == '__main__':
     app.run(debug=True)
 #ana jo3an kteer
